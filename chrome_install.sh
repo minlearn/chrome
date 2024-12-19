87e37982af5d50f5bce754eb5e07f78759fc1241
minlearnminlearn <minlearn@minlearn.org>
2024-12-20 00:28:31 +0800

    Update chrome_install.sh

--- chrome_install.sh
+++ chrome_install.sh
@@ -1,13 +1,13 @@
 ###############
 
+silent() { "$@" >/dev/null 2>&1; }
+
 echo "Installing Dependencies"
-apt-get install -y curl
-apt-get install -y sudo
-apt-get install -y mc
+silent apt-get install -y curl sudo mc
 echo "Installed Dependencies"
 
 echo "installing x11 supports"
-apt-get install --no-install-recommends debconf-utils -y -qq >/dev/null 2>&1
+silent apt-get install --no-install-recommends debconf-utils -y
 echo keyboard-configuration  keyboard-configuration/unsupported_config_options       boolean true | debconf-set-selections >/dev/null 2>&1; \
 echo keyboard-configuration  keyboard-configuration/switch   select  No temporary switch | debconf-set-selections >/dev/null 2>&1; \
 echo keyboard-configuration  keyboard-configuration/unsupported_config_layout        boolean true | debconf-set-selections >/dev/null 2>&1; \
@@ -27,7 +27,7 @@ echo keyboard-configuration  keyboard-configuration/xkb-keymap       select  us 
 echo keyboard-configuration  keyboard-configuration/optionscode      string | debconf-set-selections >/dev/null 2>&1; \
 echo keyboard-configuration  keyboard-configuration/store_defaults_in_debconf_db     boolean true | debconf-set-selections >/dev/null 2>&1
 
-apt-get install --no-install-recommends keyboard-configuration xserver-xorg xinit xterm libgtk-3-0 libwebkit2gtk-4.0-37 -y -qq >/dev/null 2>&1
+silent apt-get install --no-install-recommends keyboard-configuration xserver-xorg xinit xterm libgtk-3-0 libwebkit2gtk-4.0-37 -y
 
 chmod u+s /usr/lib/xorg/Xorg
 touch /home/tdl/.Xauthority /root/.Xauthority
@@ -51,17 +51,17 @@ echo 'exec google-chrome --no-sandbox' > /home/tdl/.xinitrc;
 systemctl enable -q --now xorg.service
 
 echo "install chrome"
-apt-get install gnupg -y
+silent apt-get install gnupg -y
 curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/google-chrome.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
-apt-get update
-apt-get install google-chrome-stable fonts-wqy-zenhei -y
-apt-get install xrdp -y
+silent apt-get update
+silent apt-get install google-chrome-stable fonts-wqy-zenhei -y
+silent apt-get install xrdp -y
 
 
 echo "Cleaning up"
-apt-get -y autoremove
-apt-get -y autoclean
+silent apt-get -y autoremove
+silent apt-get -y autoclean
 echo "Cleaned"
 
 ##############
